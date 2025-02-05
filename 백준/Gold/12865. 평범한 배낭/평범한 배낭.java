515aed18c1ad6ae99f8117983015809b2fea1c0f
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-02-05 14:04:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53780 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,37 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int n, k;
-	static int[] weight, value;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		
-		weight = new int[n];
-		value = new int[n];
-		
-		for(int i =0; i<n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			weight[i] = Integer.parseInt(st.nextToken());
-			value[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int[] dp = new int[k+1];
-		for(int i =0; i<n; i++) {
-			for(int j=k; j>=1; j--) {
-				if(weight[i]<=j) {
-					dp[j] = Math.max(dp[j], value[i]+dp[j-weight[i]]);
-				}
-			}
-		}
-		System.out.println(dp[k]);
-		br.close();
-	}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] items = new int[N+1][2];
+
+        for(int i =1; i<=N; i++){
+            st = new StringTokenizer(br.readLine());
+            items[i][0] = Integer.parseInt(st.nextToken());
+            items[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[N+1][K+1];
+
+        for(int i =1; i<=N; i++){
+            for(int w = 0; w<=K; w++) {
+                if (items[i][0] <= w) {
+                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - items[i][0]] + items[i][1]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+
+    }
 }
